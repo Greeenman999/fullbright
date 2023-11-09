86537235efd3cba887d2d609094c677fc23cfc1a
Greenman999 <mail@greenman999.de>
2023-11-09 16:14:21 +0100

    feat: added new logic for fullbright

--- src/main/java/de/greenman999/fullbright/FullbrightMod.java
+++ src/main/java/de/greenman999/fullbright/FullbrightMod.java
@@ -1,56 +1,70 @@
 package de.greenman999.fullbright;
 
-import me.shedaniel.clothconfig2.gui.entries.BooleanListEntry;
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
-import java.util.Properties;
+public class FullbrightMod implements ClientModInitializer {
 
-public class FullbrightMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("fullbright");
-	public static boolean enabled = false;
-	public static File configFolder = new File(FabricLoader.getInstance().getConfigDir().toFile() + File.separator + "fullbright-1-19");
-	public static File configFile = new File(configFolder,  "config.properties");
+
+	public static KeyBinding keyBinding;
+	private boolean loaded = false;
 
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+	public void onInitializeClient() {
+		LOGGER.info("Loaded Fullbright Mod!");
+		FConfig.HANDLER.load();
 
-		LOGGER.info("Loaded Fullbright 1.19 Mod!");
+		keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.fullbright.toggle",
+				InputUtil.Type.KEYSYM,
+				GLFW.GLFW_KEY_B,
+				"category.fullbright.main"
+		));
 
-		try {
-			if (!configFile.exists() || !configFile.canRead())
-				saveConfig();
-
-			var fis = new FileInputStream(configFile);
-			var properties = new Properties();
-			properties.load(fis);
-			enabled = Boolean.parseBoolean(properties.getProperty("enabled"));
-			fis.close();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if(!loaded && MinecraftClient.getInstance().options != null) {
+				loaded = true;
+				if(FConfig.enabled) enableFullbright();
+			}
+			while (keyBinding.wasPressed()) {
+				toggleFullbright();
+			}
+		});
 	}
 
-	public static void saveConfig() throws IOException {
-		if(!configFile.exists() || !configFile.canRead()) {
-			configFolder.mkdir();
-			configFile.createNewFile();
-		}
-		var fos = new FileOutputStream(FullbrightMod.configFile, false);
-		fos.write("# Fullbright 1.19".getBytes());
-		fos.write("\n".getBytes());
-		fos.write(("enabled=" + FullbrightMod.enabled).getBytes());
+	public static void enableFullbright() {
+		MinecraftClient.getInstance().options.getGamma().setValue(FConfig.brightness);
+		FConfig.enabled = true;
+	}
 
-		fos.close();
+	public static void disableFullbright() {
+		MinecraftClient.getInstance().options.getGamma().setValue(FConfig.defaultBrightness);
+		FConfig.enabled = false;
+	}
+
+	public void toggleFullbright() {
+		if (FConfig.enabled) {
+			disableFullbright();
+		} else {
+			enableFullbright();
+		}
+		FConfig.HANDLER.save();
+	}
+
+	public static void setFullbright(boolean enabled) {
+		if (enabled) {
+			enableFullbright();
+		} else {
+			disableFullbright();
+		}
+		FConfig.HANDLER.save();
 	}
 }
