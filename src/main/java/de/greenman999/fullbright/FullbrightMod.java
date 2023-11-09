6bef81f5cc2c2c13af998a67675921a205e3839f
Greenman999 <mail@greenman999.de>
2023-11-09 17:55:18 +0100

    feat: add /fullbright-config

--- src/main/java/de/greenman999/fullbright/FullbrightMod.java
+++ src/main/java/de/greenman999/fullbright/FullbrightMod.java
@@ -1,14 +1,26 @@
 package de.greenman999.fullbright;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.command.argument.RegistryEntryArgumentType;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
+import net.minecraft.util.Formatting;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
+import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 public class FullbrightMod implements ClientModInitializer {
 
@@ -16,6 +28,8 @@ public class FullbrightMod implements ClientModInitializer {
 
 	public static KeyBinding keyBinding;
 	private boolean loaded = false;
+	private final ModMenuApiImpl modMenuApi = new ModMenuApiImpl();
+	private boolean openConfigScreen = false;
 
 	@Override
 	public void onInitializeClient() {
@@ -29,10 +43,22 @@ public class FullbrightMod implements ClientModInitializer {
 				"category.fullbright.main"
 		));
 
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) ->
+				dispatcher.register(literal("fullbright-config")
+						.executes(context -> {
+							openConfigScreen = true;
+							return 1;
+						})
+				));
+
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if(!loaded && MinecraftClient.getInstance().options != null) {
 				loaded = true;
 				if(FConfig.enabled) enableFullbright();
+			}
+			if(openConfigScreen) {
+				openConfigScreen = false;
+				MinecraftClient.getInstance().setScreen(modMenuApi.getModConfigScreenFactory().create(MinecraftClient.getInstance().currentScreen));
 			}
 			while (keyBinding.wasPressed()) {
 				toggleFullbright();
