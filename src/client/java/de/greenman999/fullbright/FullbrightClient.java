c3c5f8bcc6312493035b519d12cce0cb5809c38e
greenman999 <mail@greenman999.de>
2025-10-03 01:46:14 +0200

    feat: implement fullbright toggle functionality and update keybindings

--- src/client/java/de/greenman999/fullbright/FullbrightClient.java
+++ src/client/java/de/greenman999/fullbright/FullbrightClient.java
@@ -1,11 +1,46 @@
 package de.greenman999.fullbright;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FullbrightClient implements ClientModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("fullbright");
+
+    private static KeyBinding keyBinding;
+
+    private static boolean toggled = false;
+
     @Override
     public void onInitializeClient() {
+        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.fullbright.toggle",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_B,
+                KeyBinding.Category.create(Identifier.of("fullbright", "main"))
+        ));
 
-        System.out.println("Fullbright Initialized");
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (keyBinding.wasPressed()) {
+                toggle();
+            }
+        });
+
+        LOGGER.info("Fullbright Initialized");
+    }
+
+    public static boolean isToggled() {
+        return toggled;
+    }
+
+    public static void toggle() {
+        toggled = !toggled;
     }
 }
---
+++ src/client/java/de/greenman999/fullbright/mixin/LightmapTextureManagerMixin.java
@@ -0,0 +1,23 @@
+package de.greenman999.fullbright.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import de.greenman999.fullbright.FullbrightClient;
+import net.minecraft.client.render.LightmapTextureManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(LightmapTextureManager.class)
+public abstract class LightmapTextureManagerMixin {
+    @WrapOperation(
+            method = "update",
+            at = @At(value = "INVOKE", target = "Ljava/lang/Double;floatValue()F", ordinal = 1)
+    )
+    private float modifyLightmap(Double instance, Operation<Float> original) {
+        if (FullbrightClient.isToggled()) {
+            return 10;
+        } else {
+            return original.call(instance);
+        }
+    }
+}
