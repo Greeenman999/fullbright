865300ad4268828b408df83bf07e3e4e5b1f95b6
greenman999 <mail@greenman999.de>
2025-10-03 01:27:37 +0200

    Purge project for a new gen.

---
+++ src/client/java/de/greenman999/fullbright/FullbrightClient.java
@@ -0,0 +1,11 @@
+package de.greenman999.fullbright;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FullbrightClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+        System.out.println("Fullbright Initialized");
+    }
+}
